76728a097ad89062c4a6bd2c77c8a44258e185fe
LukMon <monikaa.lukowska@gmail.com>
2016-07-18 15:53:00 +0200

    Added sticky menu function to the app.js file.

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoaded", function(){
-  console.log("Działa");
+$(function(){
+
+  function stickyMenu(){
+    var wholeNav = $("nav");
+    var nav = $(".nav-bar");
+    var menuTop = $(nav).offset().top;
+
+    $(window).scroll(function(){
+      var windowScroll = $(document).scrollTop();
+
+      if (windowScroll > menuTop){
+        wholeNav.addClass("sticky");
+      }else{
+        wholeNav.removeClass("sticky");
+      }
+
+      $(window).resize(function(){
+        console.log("weszło");
+        if(wholeNav.hasClass("sticky")){
+          menuTop = wholeNav.offset().top;
+          console.log("pierwszy if " + menuTop);
+        }else{
+          menuTop = nav.offset().top;
+          console.log("drugi else " + menuTop);
+        }
+      });
+
+    });
+  }
+
+  stickyMenu();
 });
